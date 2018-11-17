7f94490f78a79a84e5e92bd01ea581a970daca87
Nyan <sa@ba36.ru>
2018-11-17 20:32:30 +0300

    terraform-1

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  default = "europe-west1-b"
+}
